43888aa97c046d56b0d9e7d7b72357c7fb400d52
Amine Zeghmi <amin@MacBook-Pro-de-Amine.local>
2020-10-12 21:57:03 +0200

    Updates

---
+++ precache-manifest.e590da171713f42947cc3e711bc5c074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef7f9ae11efe483c023217d39076cd9",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "d903168482755db109bf",
+    "url": "/react-deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c81194e024c7abf269d1",
+    "url": "/react-deploy-test/static/js/2.2ba8455b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-test/static/js/2.2ba8455b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d903168482755db109bf",
+    "url": "/react-deploy-test/static/js/main.09017e15.chunk.js"
+  },
+  {
+    "revision": "d3e2f413c23f27275079",
+    "url": "/react-deploy-test/static/js/runtime-main.4964af8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
